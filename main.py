67362877bf24d9dcd7e9ff6d8202040e8058901c
bartzcarter <bartzcarter@gmail.com>
2025-02-19 19:40:57 -0700

    asteroids, movement, collisions

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # Import Statements
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     # Initialize pygame
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create two groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Add player to groups and asteroids
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    # Initialize Player and asteroid field
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
 
     # Print info 
     print("Starting Asteroids!")
@@ -16,12 +36,26 @@ def main():
 
     # Game loop
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return  
+                return
 
-        pygame.display.flip()    
+        updatable.update(dt)
+
+        for obj in asteroids:
+            if obj.collide(player):
+                print("Game Over!")
+                return
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
